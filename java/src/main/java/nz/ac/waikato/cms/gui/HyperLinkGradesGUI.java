8054903d4f0f55d0cae353e62c5f918c61e0f0ea
Peter Reutemann <fracpete@gmail.com>
2015-07-22 17:12:16 +1200

    extended GUI

--- java/src/main/java/nz/ac/waikato/cms/gui/HyperLinkGradesGUI.java
+++ java/src/main/java/nz/ac/waikato/cms/gui/HyperLinkGradesGUI.java
@@ -24,9 +24,27 @@ import nz.ac.waikato.cms.gui.core.BaseDirectoryChooser;
 import nz.ac.waikato.cms.gui.core.BaseFileChooser;
 import nz.ac.waikato.cms.gui.core.BaseFrame;
 import nz.ac.waikato.cms.gui.core.BasePanel;
+import nz.ac.waikato.cms.gui.core.BaseScrollPane;
 import nz.ac.waikato.cms.gui.core.ExtensionFileFilter;
+import nz.ac.waikato.cms.gui.core.GUIHelper;
 
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingWorker;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 /**
  * For hyperlinking pages in PDFs where Grades were found that matched a
@@ -44,6 +62,51 @@ public class HyperLinkGradesGUI
   /** the directory chooser to use. */
   protected BaseDirectoryChooser m_DirChooser;
 
+  /** the input files. */
+  protected JList m_ListInputFiles;
+
+  /** the model for the input files. */
+  protected DefaultListModel<File> m_ModelInputFiles;
+
+  /** the button for adding files. */
+  protected JButton m_ButtonAddFiles;
+
+  /** the button for removing selected files. */
+  protected JButton m_ButtonRemoveFiles;
+
+  /** the button for removing all files. */
+  protected JButton m_ButtonRemoveAllFiles;
+
+  /** the expression. */
+  protected JTextField m_TextExpression;
+
+  /** the output directory. */
+  protected JTextField m_TextOutputDir;
+
+  /** the button for selecting the output directory. */
+  protected JButton m_ButtonOutputDir;
+
+  /** the suffix. */
+  protected JTextField m_TextSuffix;
+
+  /** the checkbox for case-sensitive search. */
+  protected JCheckBox m_CheckBoxCaseSensitive;
+
+  /** the checkbox for excluding completions. */
+  protected JCheckBox m_CheckBoxExlcudeCompletions;
+
+  /** the button for indexing the files. */
+  protected JButton m_ButtonIndex;
+
+  /** the button for closing the frame. */
+  protected JButton m_ButtonClose;
+
+  /** the label for the progress. */
+  protected JLabel m_LabelProgress;
+
+  /** whether files are being processed. */
+  protected boolean m_Processing;
+
   /**
    * Initializes the members.
    */
@@ -54,13 +117,185 @@ public class HyperLinkGradesGUI
     m_FileChooser.setMultiSelectionEnabled(true);
 
     m_DirChooser = new BaseDirectoryChooser();
+
+    m_Processing = false;
   }
 
   /**
    * Initializes the widgets.
    */
   protected void initGUI() {
-    // TODO
+    JPanel	panelFiles;
+    JPanel	panelParams;
+
+    setLayout(new BorderLayout());
+
+    panelFiles = new JPanel(new BorderLayout());
+    panelFiles.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+    add(panelFiles, BorderLayout.CENTER);
+
+    // input files
+    {
+      JLabel label = new JLabel("Input files");
+      panelFiles.add(label, BorderLayout.NORTH);
+      JPanel panel = new JPanel(new BorderLayout());
+      panelFiles.add(panel, BorderLayout.CENTER);
+      m_ModelInputFiles = new DefaultListModel<>();
+      m_ListInputFiles  = new JList(m_ModelInputFiles);
+      m_ListInputFiles.addListSelectionListener(new ListSelectionListener() {
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+	  updateButtons();
+	}
+      });
+      panel.add(new BaseScrollPane(m_ListInputFiles), BorderLayout.CENTER);
+
+      JPanel panelRight = new JPanel(new BorderLayout());
+      panel.add(panelRight, BorderLayout.EAST);
+      JPanel panelButtons = new JPanel(new GridLayout(3, 1));
+      panelButtons.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 0));
+      panelRight.add(panelButtons, BorderLayout.NORTH);
+
+      m_ButtonAddFiles = new JButton("Add...");
+      m_ButtonAddFiles.addActionListener(new ActionListener() {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	  int retVal = m_FileChooser.showOpenDialog(HyperLinkGradesGUI.this);
+	  if (retVal != BaseFileChooser.APPROVE_OPTION)
+	    return;
+	  File[] files = m_FileChooser.getSelectedFiles();
+	  for (File file: files)
+	    m_ModelInputFiles.addElement(file);
+	  updateButtons();
+	}
+      });
+      panelButtons.add(m_ButtonAddFiles);
+
+      m_ButtonRemoveFiles = new JButton("Remove");
+      m_ButtonRemoveFiles.addActionListener(new ActionListener() {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	  int[] indices = m_ListInputFiles.getSelectedIndices();
+	  for (int i = indices.length - 1; i >= 0; i--)
+	    m_ModelInputFiles.remove(indices[i]);
+	  updateButtons();
+	}
+      });
+      panelButtons.add(m_ButtonRemoveFiles);
+
+      m_ButtonRemoveAllFiles = new JButton("Remove all");
+      m_ButtonRemoveAllFiles.addActionListener(new ActionListener() {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	  m_ModelInputFiles.removeAllElements();
+	  updateButtons();
+	}
+      });
+      panelButtons.add(m_ButtonRemoveAllFiles);
+    }
+
+    // the parameters
+    panelParams = new JPanel(new GridLayout(5, 1));
+    add(panelParams, BorderLayout.SOUTH);
+    {
+      // TODO
+    }
+
+    // the buttons at the bottom
+    {
+      JPanel panelBottom = new JPanel(new BorderLayout());
+      add(panelBottom, BorderLayout.SOUTH);
+
+      JPanel panelLeft = new JPanel(new FlowLayout(FlowLayout.LEFT));
+      panelBottom.add(panelLeft, BorderLayout.WEST);
+      m_LabelProgress = new JLabel("");
+      panelLeft.add(m_LabelProgress);
+
+      JPanel panelRight = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+      panelBottom.add(panelRight, BorderLayout.EAST);
+
+      m_ButtonIndex = new JButton("Index");
+      m_ButtonIndex.addActionListener(new ActionListener() {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	  indexFiles();
+	}
+      });
+      panelRight.add(m_ButtonIndex);
+
+      m_ButtonClose = new JButton("Close");
+      m_ButtonClose.addActionListener(new ActionListener() {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	  GUIHelper.closeParent(HyperLinkGradesGUI.this);
+	}
+      });
+      panelRight.add(m_ButtonClose);
+    }
+  }
+
+  /**
+   * Finishes up the initialization.
+   */
+  @Override
+  protected void finishInit() {
+    super.finishInit();
+    updateButtons();
+  }
+
+  /**
+   * Indexes the files.
+   */
+  protected void indexFiles() {
+    SwingWorker		worker;
+
+    m_Processing = true;
+    updateButtons();
+
+    worker = new SwingWorker() {
+      protected StringBuilder m_Errors;
+      @Override
+      protected Object doInBackground() throws Exception {
+	m_Errors = new StringBuilder();
+	for (int i = 0; i < m_ModelInputFiles.getSize(); i++) {
+	  File file = m_ModelInputFiles.get(i);
+	  m_LabelProgress.setText("Processing " + file.getName() + "...");
+	  try {
+
+	  }
+	  catch (Exception e) {
+	    m_Errors.append("Failed to process: " + file + "\n");
+	    System.err.println("Failed to process: " + file);
+	    e.printStackTrace();
+	  }
+	}
+	return null;
+      }
+      @Override
+      protected void done() {
+	m_LabelProgress.setText("");
+	m_Processing = false;
+	updateButtons();
+	if (m_Errors.length() > 0) {
+	  // TODO display error message
+	}
+	super.done();
+      }
+    };
+
+    worker.execute();
+  }
+
+  /**
+   * Updates the state of the buttons.
+   */
+  protected void updateButtons() {
+    m_ButtonAddFiles.setEnabled(!m_Processing);
+    m_ButtonRemoveFiles.setEnabled(!m_Processing && (m_ListInputFiles.getSelectedIndices().length > 0));
+    m_ButtonRemoveAllFiles.setEnabled(!m_Processing && (m_ModelInputFiles.getSize() > 0));
+
+    m_ButtonClose.setEnabled(!m_Processing);
+    m_ButtonIndex.setEnabled(!m_Processing && (m_ModelInputFiles.getSize() > 0));
   }
 
   /**
--- java/src/main/java/nz/ac/waikato/cms/gui/core/GUIHelper.java
+++ java/src/main/java/nz/ac/waikato/cms/gui/core/GUIHelper.java
@@ -21,6 +21,14 @@
 package nz.ac.waikato.cms.gui.core;
 
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dialog;
+import java.awt.Frame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 /**
  * Helper class for GUI related stuff.
@@ -56,4 +64,175 @@ public class GUIHelper {
       return null;
   }
 
+  /**
+   * Tries to determine the parent this panel is part of.
+   *
+   * @param cont	the container to get the parent for
+   * @param parentClass	the class of the parent to obtain
+   * @return		the parent if one exists or null if not
+   */
+  public static Object getParent(Container cont, Class parentClass) {
+    Container	result;
+    Container	parent;
+
+    result = null;
+
+    parent = cont;
+    while (parent != null) {
+      if (parentClass.isInstance(parent)) {
+	result = parent;
+	break;
+      }
+      else {
+	parent = parent.getParent();
+      }
+    }
+
+    return result;
+  }
+
+  /**
+   * Tries to determine the frame the container is part of.
+   *
+   * @param cont	the container to get the frame for
+   * @return		the parent frame if one exists or null if not
+   */
+  public static Frame getParentFrame(Container cont) {
+    return (Frame) getParent(cont, Frame.class);
+  }
+
+  /**
+   * Tries to determine the frame the component is part of.
+   *
+   * @param comp	the component to get the frame for
+   * @return		the parent frame if one exists or null if not
+   */
+  public static Frame getParentFrame(Component comp) {
+    if (comp instanceof Container)
+      return (Frame) getParent((Container) comp, Frame.class);
+    else
+      return null;
+  }
+
+  /**
+   * Tries to determine the dialog this container is part of.
+   *
+   * @param cont	the container to get the dialog for
+   * @return		the parent dialog if one exists or null if not
+   */
+  public static Dialog getParentDialog(Container cont) {
+    return (Dialog) getParent(cont, Dialog.class);
+  }
+
+  /**
+   * Tries to determine the dialog this component is part of.
+   *
+   * @param comp	the component to get the dialog for
+   * @return		the parent dialog if one exists or null if not
+   */
+  public static Dialog getParentDialog(Component comp) {
+    if (comp instanceof Container)
+      return (Dialog) getParent((Container) comp, Dialog.class);
+    else
+      return null;
+  }
+
+  /**
+   * Tries to determine the internal frame this container is part of.
+   *
+   * @param cont	the container to start with
+   * @return		the parent internal frame if one exists or null if not
+   */
+  public static JInternalFrame getParentInternalFrame(Container cont) {
+    return (JInternalFrame) getParent(cont, JInternalFrame.class);
+  }
+
+  /**
+   * Tries to determine the internal frame this component is part of.
+   *
+   * @param comp	the component to start with
+   * @return		the parent internal frame if one exists or null if not
+   */
+  public static JInternalFrame getParentInternalFrame(Component comp) {
+    if (comp instanceof Container)
+      return (JInternalFrame) getParent((Container) comp, JInternalFrame.class);
+    else
+      return null;
+  }
+
+  /**
+   * Tries to determine the component this panel is part of in this order:
+   * 1. dialog, 2. child, 3. frame.
+   *
+   * @param comp	the component to get the parent component for, must
+   * 			be a container actually
+   * @return		the parent component if one exists or null if not
+   * @see		#getParentDialog(Container)
+   * @see		#getParentFrame(Container)
+   */
+  public static Component getParentComponent(Component comp) {
+    Component	result;
+    Container	cont;
+
+    if (comp == null)
+      return null;
+
+    if (comp instanceof Container)
+      cont = (Container) comp;
+    else
+      return null;
+
+    result = getParentDialog(cont);
+    if (result == null)
+      result = getParentFrame(cont);
+
+    return result;
+  }
+
+  /**
+   * Closes the parent dialog/frame of this container.
+   *
+   * @param cont	the container to close the parent for
+   */
+  public static void closeParent(Container cont) {
+    Dialog		dialog;
+    Frame		frame;
+    JFrame jframe;
+    JInternalFrame	jintframe;
+    int		i;
+    WindowListener[] 	listeners;
+    WindowEvent event;
+
+    if (getParentDialog(cont) != null) {
+      dialog = getParentDialog(cont);
+      dialog.setVisible(false);
+    }
+    else if (getParentFrame(cont) != null) {
+      jintframe = getParentInternalFrame(cont);
+      if (jintframe != null) {
+	jintframe.doDefaultCloseAction();
+      }
+      else {
+	frame = getParentFrame(cont);
+	if (frame instanceof JFrame) {
+	  jframe = (JFrame) frame;
+	  if (jframe.getDefaultCloseOperation() == JFrame.HIDE_ON_CLOSE)
+	    jframe.setVisible(false);
+	  else if (jframe.getDefaultCloseOperation() == JFrame.DISPOSE_ON_CLOSE)
+	    jframe.dispose();
+	  else if (jframe.getDefaultCloseOperation() == JFrame.EXIT_ON_CLOSE)
+	    System.exit(0);
+
+	  // notify listeners
+	  listeners = jframe.getWindowListeners();
+	  event     = new WindowEvent(jframe, WindowEvent.WINDOW_CLOSED);
+	  for (i = 0; i < listeners.length; i++)
+	    listeners[i].windowClosed(event);
+	}
+	else {
+	  frame.dispose();
+	}
+      }
+    }
+  }
 }
